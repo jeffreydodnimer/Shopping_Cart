ebab53b5c2e0c22bf906a987f1463efc6fd262bb
jeffreydodnimer <jeffreyodnimer31@gmail.com>
2025-05-26 01:29:42 +0800

    pay_items

--- pay_items.py
+++ pay_items.py
@@ -5,8 +5,8 @@ def run(cart):
 
     print("\n💳 Pay for Items")
     for i, item in enumerate(cart.items, 1):
-        print(f"{i}. {item['name'].title()} | Qty: {item['quantity']} | Price: "
-              f"P{item['price']:.2f}")
+        print(f"{i}. {item['name'].title()} | Qty: {item['quantity']} | "
+              f"Price: P{item['price']:.2f}")
 
     total = 0
     while True:
